adc4b1f96c29aa3228a144f1abf76233455d9f55
[4기-1반]김애림 <pinkpig_21@naver.com>
2022-05-24 17:12:59 +0900

    1966번

--- 김애림/1주차/1966번.py
+++ 김애림/1주차/1966번.py
@@ -3,6 +3,28 @@
 
 import sys
 
-T = map(int, sys.stdin.readline().strip())
+T = int(sys.stdin.readline())
 
-# 중요도 확인하기
+for _ in range(T):
+    # x는 프린터 기기로 인쇄할 문서 개수
+    # y는 궁금한 문서의 위치
+    x, y = list(map(int, sys.stdin.readline().split()))
+    imp = list(map(int, sys.stdin.readline().split()))
+    order = 0
+
+    while 1:
+        if imp[0] == max(imp):
+            order += 1
+            if y == 0:
+                print(order)
+                break
+            else:
+                y -= 1
+                imp.pop(0)
+        else:
+            imp.append(imp[0])
+            imp.pop(0)
+            if y == 0:
+                y = len(imp)-1
+            else:
+                y -= 1
