b23af89b8a032981e402abf19df3c8c6d1eab519
aldrn <aldrn29@gmail.com>
2022-05-25 23:37:37 +0900

    1966 프린터 큐

--- 안민영/1주차/1966 [프린터 큐].py
+++ 안민영/1주차/1966 [프린터 큐].py
@@ -4,9 +4,9 @@ from sys import stdin
 num = int(stdin.readline()) 
 for i in range(num) :
     n, m = map(int, stdin.readline().split()) # 문서의 개수, 인쇄할거 몇번째 놓여있는지 
-    queue = deque(map(int, stdin.readline().split())) # 1 1 9 1 1 1
+    queue = deque(map(int, stdin.readline().split()))
     cnt = 0
-    curr = m
+    curr = m # 인쇄할 문서의 위치
 
     while queue :
         max_priority = max(queue) # 1
