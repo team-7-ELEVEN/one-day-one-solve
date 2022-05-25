5a09b81ec37b6cd231d780544f236ebcc690ed55
nowgnas <marshmello.sw@gmail.com>
2022-05-25 22:54:00 +0900

    0525상원-프린터큐

--- 이상원/1주차/프린터_큐.py
+++ 이상원/1주차/프린터_큐.py
@@ -10,18 +10,28 @@ https://www.acmicpc.net/problem/1966
 1 1 9 1 1 1
 
 '''
-import heapq
+import collections
 import sys
 
 
 def print_docs(n, m, score: list):
-    arr = []
+    deq = collections.deque()
+    for idx, item in enumerate(score):
+        deq.append((idx, item))
     answer = 0
-    for idx, val in enumerate(score):
-        arr.append((val, idx))
-
     while True:
-        max_val =
+        max_val = max(deq, key=lambda x: x[1])
+        max_val = max_val[1]
+        if max_val != deq[0][1]:
+            pop_tuple = deq.popleft()
+            deq.append(pop_tuple)
+        elif max_val == deq[0][1] and m != deq[0][0]:
+            deq.popleft()
+            answer += 1
+        elif max_val == deq[0][1] and m == deq[0][0]:
+            answer += 1
+            break
+    return answer
 
 
 if __name__ == '__main__':
