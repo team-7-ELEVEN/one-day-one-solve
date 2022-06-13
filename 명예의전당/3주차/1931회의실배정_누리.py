75c3bb7c3855bedf6290a7bbbb38eef480d5ce08
nowgnas <marshmello.sw@gmail.com>
2022-06-13 23:05:26 +0900

    add 명예의 전당

--- 명예의전당/3주차/1931회의실배정_누리.py
+++ 명예의전당/3주차/1931회의실배정_누리.py
@@ -1,23 +1,3 @@
-# n=int(input())
-# cnt=0
-# task=[]
-# time=[]
-
-# for _ in range(n):
-#     start,end=map(int,input().split())
-#     task.append((start,end))
-
-# task=sorted(task,key=lambda x:(x[1]-x[0]))
-
-# for t in task:
-#     if t[0] not in time and t[1] not in time:
-#         cnt+=1
-#         for i in range(t[0],t[1]+1):
-#             time.append(i)
-
-# print(cnt)
-
-# 위에 코드는 시간초과!
 
 n = int(input())
 cnt = 0
