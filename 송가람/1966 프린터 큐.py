1caf32be6f3ee7994f626735e73b22b6423c0c77
songgaram <pager1436@gmail.com>
2022-08-05 23:58:18 +0900

    1966 수정

--- 송가람/1966 프린터 큐.py
+++ 송가람/1966 프린터 큐.py
@@ -5,22 +5,20 @@ def printer_queue():
     N, M = map(int, input().split())
     dq = deque(list(map(int, input().split())))
     cnt = 0
-    if N == 1:
-        return 1
-    else:
-        for i in range(N):
-            dq[i] = (dq[i], i)
+  
+    for i in range(N):
+        dq[i] = (dq[i], i)
 
-        while True:
-            if dq[0][0] == max(dq)[0]:
-                cnt += 1
-                if dq[0][1] == M:
-                    return cnt
-                    break
-                else:
-                    dq.popleft()
+    while True:
+        if dq[0][0] == max(dq)[0]:
+            cnt += 1
+            if dq[0][1] == M:
+                return cnt
+                break
             else:
-                dq.append(dq.popleft())
+                dq.popleft()
+        else:
+            dq.append(dq.popleft())
 
 
 for _ in range(n):
