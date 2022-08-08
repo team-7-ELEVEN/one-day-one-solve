b574a605657de04e62ba854f205a455d29f0abb4
노누리 <noori105066@gmail.com>
2022-08-08 16:21:43 +0900

    1654.java

--- 노누리/class2++/1654.java
+++ 노누리/class2++/1654.java
@@ -25,7 +25,7 @@ public class BJ1654 {
 
         // 이분탐색
         Collections.sort(arr);
-        int left=0;
+        int left=1;
         int right=arr.get(arr.size()-1);
         int mid=0;
 
@@ -42,12 +42,12 @@ public class BJ1654 {
             }
 
             if(cnt<N)
-                right=mid;
+                right=mid-1;
             else
                 left=mid+1;
         }
 
-        System.out.println(mid-1);
+        System.out.println(right);
         /*
         // 시간초과
         while(cnt<N){
@@ -69,3 +69,36 @@ public class BJ1654 {
          */
     }
 }
+
+/*
+* import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+        long N = sc.nextLong();
+        long[] arr = new long[K];
+        long max = 0;
+        for (int i = 0; i < K; i++) {
+            arr[i] = sc.nextLong();
+            max = Math.max(max, arr[i]);
+        }
+        //이분탐색
+        long left = 1; //랜선길이는 자연수므로  최솟값 1로 세팅해야함
+        long right = max;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long sum = 0;
+            for (int i = 0; i < K; i++) { // 자른 개수 합
+                sum += (arr[i] / mid);
+            }
+            if (sum >= N) { //더 많은 랜선 나온 경우 더 크게 잘라줘야함
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println(right);
+    }
+}*/
