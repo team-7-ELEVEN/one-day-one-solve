932aee938544e37ab40ab76d8f5047d398b5ff0b
songgaram <pager1436@gmail.com>
2022-08-09 22:09:28 +0900

    4949 균형잡힌 세상

---
+++ 송가람/class2/2775 부녀회장이 될테야.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+k, n = map(int, input().split())
+
+def cal_resident(floor, room):
+    people = []
+    for _ in range(floor):
+        for i in range(1, room + 1):
+            people.append()
+    return people
+
+print(cal_resident(k, n))
+
+# 못해먹겠네
+
