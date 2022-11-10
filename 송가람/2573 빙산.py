69eed5dceda564a29b19ba3f37fb0af66b1fb95f
songgaram <pager1436@gmail.com>
2022-11-10 20:46:11 +0900

    array renaming

--- 송가람/2573 빙산.py
+++ 송가람/2573 빙산.py
@@ -11,30 +11,30 @@ def bfs(Y, X):
     global graph
     q = deque()
     q.append([Y, X])
-    update[Y][X] = True
+    visited[Y][X] = True
     while q:
         y, x = q.popleft()
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
-            if 0 <= ny < N and 0 <= nx < M and not update[ny][nx]:
+            if 0 <= ny < N and 0 <= nx < M and not visited[ny][nx]:
                 if graph[ny][nx] == 0:
                     graph[y][x] -= 1
                     if graph[y][x] < 0:
                         graph[y][x] = 0
                 else:
                     q.append([ny, nx])
-                    update[ny][nx] = True
+                    visited[ny][nx] = True
 
 year = 0
 while True:
     # cnt를 초기화해야함 => 아니면 매년 마다 cnt값이 갱신됨
     cnt = 0
     year += 1
-    update = [[False] * M for _ in range(N)]
+    visited = [[False] * M for _ in range(N)]
     for i in range(N):
         for j in range(M):
-            if graph[i][j] != 0 and not update[i][j]:
+            if graph[i][j] != 0 and not visited[i][j]:
                 cnt += 1
                 bfs(i, j)
     
