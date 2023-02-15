79353f86f20e7a2d2dce358d04063cb9c65ce18d
songgaram <pager1436@gmail.com>
2023-02-15 16:38:22 +0900

    10973 이전수열 복습

---
+++ 송가람/implement/10973 이전수열.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+k = -1
+for i in range(len(arr) - 1):
+    if arr[i] > arr[i + 1]:
+        k = i
+
+if k == -1:
+    print(-1)
+else:
+    for i in range(k + 1, len(arr)):
+        if arr[i] < arr[k]:
+            m = i
+
+    arr[k], arr[m] = arr[m], arr[k]
+
+    temp = arr[k + 1 :]
+    temp.sort(reverse=True)
+    answer = arr[: k + 1] + temp
+
+    for num in answer:
+        print(num, end=" ")
